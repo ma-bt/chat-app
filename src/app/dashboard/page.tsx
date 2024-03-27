3e3a1dda5ec28ef8c5aab65e1a3d7005dd8f51fb
Mandira Basnet <mandeerab20@gmail.com>
2024-03-27 23:47:10 +0545

    feat: :memo: Button component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = () => {
+  return <Button> Click </Button>;
+};
+
+export default page;
